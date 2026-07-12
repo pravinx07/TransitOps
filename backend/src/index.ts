0db55995f4dfac330c3e0b2fa5da81937ba8cd89
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-07-12 14:19:59 +0530

    built vehicle functionality and perfomr crud operation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./modules/auth/auth.routes";
+import vehicleRoutes from "./modules/vehicles/vehicles.routes";
 import { sendError } from "./utils/apiResponse";
 
 // Load environment variables
@@ -16,6 +17,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/vehicles", vehicleRoutes);
 
 // Test Route
 app.get("/health", (req, res) => {
